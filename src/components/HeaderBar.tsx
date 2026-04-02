968d12807c92e8c733bb29cc2c2d7e94b397a799
wiktorspryszynski <spryszynskiwiktor@gmail.com>
2026-04-02 15:31:55 +0200

    logo animation and redirect

--- src/components/HeaderBar.tsx
+++ src/components/HeaderBar.tsx
@@ -6,6 +6,25 @@ interface HeaderBarProps {
   brandAriaLabel?: string;
 }
 
+const redirectHome = () => {
+  window.location.href = window.location.origin;
+}
+
+const updateHomeLinkLight = (event: React.MouseEvent<HTMLDivElement>) => {
+  const rect = event.currentTarget.getBoundingClientRect();
+  const x = event.clientX - rect.left;
+  const y = event.clientY - rect.top;
+
+  event.currentTarget.style.setProperty('--home-light-x', `${x}px`);
+  event.currentTarget.style.setProperty('--home-light-y', `${y}px`);
+}
+
+const resetHomeLinkLight = (event: React.MouseEvent<HTMLDivElement>) => {
+  const rect = event.currentTarget.getBoundingClientRect();
+  event.currentTarget.style.setProperty('--home-light-x', `${rect.width / 2}px`);
+  event.currentTarget.style.setProperty('--home-light-y', `${rect.height / 2}px`);
+}
+
 const HeaderBar: React.FC<HeaderBarProps> = ({
   title,
   switchDisplay,
@@ -17,7 +36,16 @@ const HeaderBar: React.FC<HeaderBarProps> = ({
     <header role="banner">
       <div className="container header-bar">
         <div className="brand" aria-label={brandAriaLabel}>
-          <div className="name-and-surname">{title}</div>
+          <div
+            className="name-and-surname"
+            id="home-link"
+            onClick={redirectHome}
+            onMouseEnter={updateHomeLinkLight}
+            onMouseMove={updateHomeLinkLight}
+            onMouseLeave={resetHomeLinkLight}
+          >
+            {title}
+          </div>
         </div>
         <nav>
           <button
