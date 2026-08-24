2ba829eb919f6a2824571a12256f454751ba47b8
wiktorspryszynski <spryszynskiwiktor@gmail.com>
2026-08-24 11:04:00 +0200

    feat: update hiring status and resume

--- src/config/features.ts
+++ src/config/features.ts
@@ -1,4 +1,4 @@
 export const BACKEND_EXISTS = false
-export const LOOKING_FOR_HIRE = false
+export const LOOKING_FOR_HIRE = true
 export const SPIKES_ENABLED = true
 export const NOW_PAGE_AVAILABLE = false
